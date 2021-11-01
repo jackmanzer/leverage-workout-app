2f47a2dcb2bd7aa468e5e843f59980606af5e96f
BradMW <bodell94@yahoo.com>
2021-11-01 11:57:45 -0500

    sql set up

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO user (user_title, user_password, email)
+VALUES ("Rock", "pWord123", "rock@on.com")
+
+
+INSERT INTO workout (workout_name)
+VALUES ("Squats") 
+
+INSERT INTO exercises (exercise_name, exercise_set, exercise_reps, exercise_weight, workout_id)
+VALUES ("Squats", 4, 50, 200, 1)
+
+
+-- INSERT INTO sevenDay (day, workout, workout_id)
+-- VALUES ("Sunday", "", ""),
+--        ("Monday", "", ""),
+--        ("Tuesday", "", ""),
+--        ("Wednesday", "", ""),
+--        ("Thursday", "", ""),
+--        ("Friday", "", ""),
+--        ("Saturday", "", "")
+       
+
+
