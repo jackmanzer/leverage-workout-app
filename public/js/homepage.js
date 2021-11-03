e95c091273a35ac7f186697f1141f602ab693d36
jayladenae <jaydenaen@icloud.com>
2021-11-03 10:25:38 -0500

    added tailwind link and added homepage button functions

---
+++ public/js/homepage.js
@@ -0,0 +1,14 @@
+const signUpBtn = document.querySelector('#signUpBtn');
+ const signInBtn =  document.querySelector('#signInBtn');
+
+ signInBtn.addEventListener('click', function() {
+    document.getElementById('signInForm').style.display = "block";
+         signUpBtn.style.display = "none";
+    signInBtn.style.display = "none";
+});
+
+signUpBtn.addEventListener('click', function() {
+    document.getElementById('signUpForm').style.display = "block";
+    signUpBtn.style.display = "none";
+    signInBtn.style.display = "none";
+});
