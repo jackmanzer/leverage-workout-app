437adde48ea46d5e2248eca0c2a1011045e5da8d
BradMW <bodell94@yahoo.com>
2021-11-04 15:37:18 -0500

    uncommented out buttons that were needed

--- public/js/workouts.js
+++ public/js/workouts.js
@@ -1,17 +1,17 @@
-// const assignDayBtn = document.querySelector("#assignDayBtn");
-// const exerciseNameButton = document.querySelector("#exerciseNameButton");
+const assignDayBtn = document.querySelector("#assignDayBtn");
+const exerciseNameButton = document.querySelector("#exerciseNameButton");
 
-// assignDayBtn.addEventListener('click', function(event) {
-//     event.preventDefault()
-//     document.getElementById('assignDayForm').style.display = "block";
-//     assignDayBtn.style.display ="none";
-// });
+assignDayBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    document.getElementById('assignDayForm').style.display = "block";
+    assignDayBtn.style.display ="none";
+});
 
-// exerciseNameButton.addEventListener('click', function(event) {
-//     event.preventDefault()
-//     document.getElementById('hiddenForm').style.display ="block";
-//     exerciseNameButton.style.display = "none";
-// });
+exerciseNameButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    document.getElementById('hiddenForm').style.display ="block";
+    exerciseNameButton.style.display = "none";
+});
 
 const addExerciseForm = async (event) => {
     event.preventDefault();
