de776bf687b96eb93e867fc32d7b8ce03a670661
jack <jackmanzer88@gmail.com>
2021-11-03 19:17:20 -0500

    wired up schedule and workout handlebars

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -2,48 +2,42 @@ const router = require('express').Router();
 const { Workout } = require('../../models');
 const { Exercise } = require('../../models');
 
-router.get('/', async (req, res) => {
-    try {
-      const workoutData = await Workout.findAll();
-      res.status(200).json(workoutData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//       const workoutData = await Workout.findAll();
+//       res.status(200).json(workoutData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+// });
 
 router.get('/:id', async (req, res) => {
+
   try {
     const workoutData = await Workout.findByPk(req.params.id, {
       // JOIN with products
       include: [{ model: Exercise }]
     });
 
+    console.log(workoutData);
+
     if (!workoutData) {
       res.status(404).json({ message: 'No workout found with this id!' });
       return;
     }
 
-    res.status(200).json(workoutData);
+    // Serialize data so the template can read it
+    const workout = workoutData.get({ plain: true });
+
+    console.log(workout)
+
+    res.render('workouts', {
+      ...workout,
+    })
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-router.get('/:id', async (req, res) => {
-    try {
-      const workoutData = await Workout.findByPk(req.params.id, {
-        include: [{ model: Exercise }]
-      });
-      if (!workoutData) {
-        res.status(404).json({ message: 'No workout found with this id!' });
-        return;
-      }
-      res.status(200).json(workoutData);
-    } catch (err) {
-      res.status(500).json(err);
-      console.log(err);
-    }
-  });
 
 router.post('/', async (req, res) => {
     try {
